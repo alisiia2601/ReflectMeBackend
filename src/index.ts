a34fe8c435a69b0b0fdd3f73e2829500922fff2c
alisiia2601 <alisiia.ryzhykova@icloud.com>
2024-01-25 00:28:51 +0100

    new

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import app from "./app";
+
+const mongoURL = process.env.DB_URL;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+if (!mongoURL) throw Error("Missing db url");
+
+mongoose.connect(mongoURL).then(() => {
+  const port = parseInt(process.env.PORT || "3000");
+  app.listen(port, () => {
+    console.log("Server listening on port " + port);
+  });
+});
